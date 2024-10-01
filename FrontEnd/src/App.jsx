05289bcad1676bd117f84059c8a150dded44a3af
Rakxit-Shetty <rakshitshetty136@gmail.com>
2024-10-01 21:38:38 +0530

    inited fntEnd

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [empName, setEmpName] = useState("");
+  const [empAge,setEmpAge]=useState("");
+
+  const [empList,setEmpList]=useState([])
+
+  useEffect(()=>{
+
+    axios.get('/user?ID=12345').then((data)=>{
+      setEmpList((e)=>([...e,data]))
+    })
+  })
+  
+  console.log("empList",empList);
 
   return (
-    <>
+    <div class="container">
+      <div className="d-flex justify-content-center align-item-center">
+        <div >
+          <h2 className="mb-4">Employee DataStore</h2>
+        <div>
+          <label>Employee Name</label>
+        <input id="name" name="empName" className="mx-2" type="text" value={empName} onChange={(e)=>setEmpName(e.target.value)}/>
+
+        </div>
+        <div> <label>Employee Name</label>
+        <input id="age" name="age" className="mx-2" type="text" value={empAge} onChange={(e)=>setEmpAge(e.target.value)}/>
+
+        </div>
+      <button type="submit" className="btn btn-success float-right my-3">ADD</button>
+      </div>
+      
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <table>
+        <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Age</th>
+  </tr>
+  {
+    empList.map((ele,idx)=>{
+return <>
+  <tr>
+    <td>{"ID"}</td>
+    <td>{"name"}</td>
+    <td>{"age"}</td>
+  </tr>
+</>
+    })
+  }
+
+  
+        </table>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
