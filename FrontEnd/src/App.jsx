73d94a0241b9ca5be5217ca728d00f352f1eb394
Rakxit-Shetty <rakshitshetty136@gmail.com>
2024-10-01 21:39:00 +0530

    inited new frntEnd

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,45 +13,58 @@ function App() {
 
   useEffect(()=>{
 
-    axios.get('/user?ID=12345').then((data)=>{
-      setEmpList((e)=>([...e,data]))
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((res)=>{
+      console.log(res.data)
+      setEmpList((e)=>([...e,...res.data]))
     })
-  })
+  },[])
   
   console.log("empList",empList);
 
   return (
     <div class="container">
       <div className="d-flex justify-content-center align-item-center">
-        <div >
+        <div>
           <h2 className="mb-4">Employee DataStore</h2>
         <div>
-          <label>Employee Name</label>
+
+          
+        <div className="row col-12">
+          
+          <div className="col-6"> 
+           
+            <label>Employee Name</label>
         <input id="name" name="empName" className="mx-2" type="text" value={empName} onChange={(e)=>setEmpName(e.target.value)}/>
-
+        
         </div>
-        <div> <label>Employee Name</label>
+        <div className="col-6"><label>Employee Age</label>
         <input id="age" name="age" className="mx-2" type="text" value={empAge} onChange={(e)=>setEmpAge(e.target.value)}/>
-
         </div>
+        </div>
+        
+         
+        </div>
+        
       <button type="submit" className="btn btn-success float-right my-3">ADD</button>
       </div>
       
       </div>
       <div>
         <table>
-        <tr>
+        <tr className="bg-sucess">
     <th>ID</th>
     <th>Name</th>
     <th>Age</th>
+    <th>Action</th>
   </tr>
   {
     empList.map((ele,idx)=>{
 return <>
   <tr>
-    <td>{"ID"}</td>
-    <td>{"name"}</td>
-    <td>{"age"}</td>
+    <td>{ele.userId}</td>
+    <td>{ele.title}</td>
+    <td>{ele.userId}</td>
+    <td><button className="btn btn-primary mx-2">Edit</button><button className="btn btn-danger mx-2">Del</button></td>
   </tr>
 </>
     })
